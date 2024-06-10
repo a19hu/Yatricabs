ac4cd7bb51c97628eab09f2f5e0436c0e26ed3a6
a19hu <a19hu20osh1jkl@gmail.com>
2024-06-10 17:16:32 +0530

    add template of web

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './components/Pages/Home';
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
